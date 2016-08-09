39705a2a67c83b1594b40d27b12715c6df1183c4
Stoyan Stoyanov <stoyan.stoyanov@weltn24.de>
2016-08-09 10:09:34 +0200

    Revert "dimension.name and dimension.value are now dimensions"
    This reverts commit 159243da7298ddb256e73517cda8f1ac40e0d382.

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"strings"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -18,10 +17,11 @@ func main() {
 	var (
 		namespace          = flag.String("namespace", "", "metric namespace, must not be empty")
 		metricName         = flag.String("metric.name", "", "metric name, must not be empty")
+		dimensionName      = flag.String("dimension.name", "", "metric dimension name, must not be empty")
+		dimensionValue     = flag.String("dimension.value", "", "metric dimension value, must not be empty")
 		monitoringDetailed = flag.Bool("detailed", false, "monitoring resoution: 1m (else: 5m)")
 		statistics         = flag.String("stats", "Average", "possible values: Minimum, Maximum, Average, Sum, SampleCount")
 		awsRegion          = flag.String("aws.region", "eu-central-1", "AWS region")
-		dimensions         = flag.String("dimensions", "", "dimension name and dimension value")
 		period             = 5 * time.Minute
 	)
 	flag.Parse()
@@ -31,15 +31,9 @@ func main() {
 	}
 	startTime := time.Now().Add(-1 * period)
 
-	if !statsMap[*statistics] || *namespace == "" || *dimensions == "" {
+	if !statsMap[*statistics] || *namespace == "" || *dimensionName == "" || *dimensionValue == "" {
 		flag.Usage()
 		return
-	}
-
-	err, d := parseDimensions(*dimensions)
-
-	if err != nil {
-		fmt.Println(err)
 	}
 
 	cloudwatchCli := cloudwatch.New(session.New(), aws.NewConfig().WithRegion(*awsRegion))
@@ -51,7 +45,10 @@ func main() {
 		Period:     aws.Int64(int64(period.Seconds())),
 		StartTime:  &startTime,
 		Statistics: []*string{statistics},
-		Dimensions: d,
+		Dimensions: []*cloudwatch.Dimension{{
+			Name:  dimensionName,
+			Value: dimensionValue,
+		}},
 	}
 	resp, err := cloudwatchCli.GetMetricStatistics(&params)
 	if err != nil {
@@ -77,17 +74,4 @@ func main() {
 	} else {
 		fmt.Println("nodata")
 	}
-}
-
-func parseDimensions(dimensions string) (error, []*cloudwatch.Dimension) {
-	splitted := strings.Split(dimensions, ":")
-
-	if len(splitted) != 2 {
-		return fmt.Errorf("Parsing dimensions failed, there should be two values, got %v. Check you format. Should be dValue:dName", len(splitted)), nil
-	}
-
-	return nil, []*cloudwatch.Dimension{{
-		Name:  &splitted[0],
-		Value: &splitted[1],
-	}}
 }
